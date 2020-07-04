8508ca47081220e5628ca59a0d89de296c154b8d
puneetsinghania <puneetsinghania2002@gmail.com>
2020-07-04 21:55:19 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         I was balle noballe
+         I was balle noballe chota baba feature branch
         </p>
         <a
           className="App-link"
